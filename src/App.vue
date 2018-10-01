bbc19ebd6c095e61aea7bdf36c0bf7db0fa4c05c
MaxEzaki <takeru.info@gmail.com>
2018-10-02 08:22:13 +0900

    画像パス

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
     <Layout>
       <Header :style="{background: '#fff', boxShadow: '0 2px 3px 2px rgba(0,0,0,.1)'}">
         <div class="layout-logo">
-          <!-- <img src="/assets/img/icon_max.png" alt="ロゴ"> -->
-          <img :src="imgPathLogo" alt="ロゴ">
+          <img src="./assets/img/icon_max.png" alt="ロゴ">
+          <!-- <img :src="imgPathLogo" alt="ロゴ"> -->
         </div>
       </Header>
         <Content class="content">
