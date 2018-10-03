ed4f4fc417143a6ba104d17fc8928a1c6a3823b9
maxKT <max.perc+sys@gmail.com>
2018-10-04 07:21:58 +0900

    フォーム作成中

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,24 @@
             <div class="is-center">
               <img src="./assets/image/img_max.jpg">
               <h3>Hi, this is Max_Ezaki.  Feel free to contact me :)</h3>
+<Form ref="formContact" :model="formContact" label-position="top" :rules="ruleValidate" name="contact" netlify>
+                    <FormItem label="Name" prop="name">
+                      <Input v-model="formContact.name" placeholder="Enter your name" name="name"></Input>
+                    </FormItem>
+                    <FormItem label="Email" prop="email">
+                      <Input v-model="formContact.email" placeholder="Enter your email" name="email"></Input>
+                    </FormItem>
+                    <FormItem label="Desc" prop="desc">
+                        <Input v-model="formContact.desc" type="textarea" :autosize="{minRows: 5,maxRows: 10}" placeholder="Enter something..."></Input>
+                    </FormItem>
+                    <FormItem>
+                        <Button type="primary" @click="handleSubmit('formContact')">Submit</Button>
+                    </FormItem>
+
+                    <p>
+                      <button type="submit">Send</button>
+                    </p>
+                  </Form>
             </div>
             <Button @click="modalContact = true">Contact</Button>
               <Modal
