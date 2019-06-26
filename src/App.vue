8c36e1dc1af43774d56527586a8418b56ee53f64
maxKT <max.perc+sys@gmail.com>
2019-06-26 22:49:40 +0900

    aa

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,45 @@
 <template>
   <div id="app">
-<section class="section section-header bg-gray">
-	<section class="grid-header container grid-960 text-center">
-<!--
-				<figure class="avatar avatar-xl">
-					<img src="images/logo_max00.png">
-				</figure>
--->
-				<p>maxezaki.com</p>
-					</section>
-</section>
-
-<div class="col-5 col-xs-11 wrapper">
-	<div class="panel">
-	  <div class="panel-header text-center">
-		  <figure class="avatar avatar-lg"><img src="images/icon_max00.jpg"></figure>
-	    <div class="panel-title mt-10">Max_Ezaki</div>
-	  </div>
-	  <div class="panel-nav">
-	    <ul class="tab tab-block">
-		  <li class="tab-item active">
-		    <a href="#">Profile</a>
-		  </li>
-<!--
-		  <li class="tab-item">
-		    <a href="#" class="active">Playlists</a>
-		  </li>
-		  <li class="tab-item">
-		    <a href="#">Radio</a>
-		  </li>
-		  <li class="tab-item">
-		    <a href="#">Connect</a>
-		  </li>
--->
-		</ul>
-	  </div>
-	  <div class="panel-body">
-	    <!-- contents -->
-	    <p>
-		    
-		    Hi, this is Max Ezaki. Feel free to contact me :)<br>
-								     
-								      <a href="https://www.pakutaso.com/Max_Ezaki.html">#stockphotomodel</a> <a>#webengineer</a> <a>#mc</a> <a>#german</a> <a>#japanese</a> <a href="https://www.pakutaso.com/search.html?offset=0&limit=30&search=%E3%83%89%E3%82%A4%E3%83%84%E4%BA%BA%E3%83%8F%E3%83%BC%E3%83%95">#現役ドイツ人ハーフ</a> <a href="https://www.pakutaso.com/">#ぱくたそ</a> <a href="#">#japnese</a> <a href="#">#english</a> <a href="#">#german</a>
-	    </p>
-	    
-	    
-	  </div>
-	  <div class="panel-footer">
-	    <!-- buttons or inputs -->
-	  </div>
-	</div>
-</div>
-
-<footer class="bg-gray footer">
-	<section class="grid-footer container grid-960">
-			<div class="columns">
-			  <div class="column text-center"><a href="https://www.facebook.com/rhythmonchi/"><span class="icon"><i class="fa fa-facebook" aria-hidden="true"></i></span></a></div>
-			  <div class="column text-center"><a href="https://twitter.com/Max_Ezaki"><span class="icon"><i class="fa fa-twitter" aria-hidden="true"></i></span></a></div>
-			  <div class="column text-center"><a href="http://maxezaki.photo"><span class="icon"><i class="fa fa-tumblr" aria-hidden="true"></i></span></a></div>
-			  <div class="column text-center"><a href="https:/rhythm-onchi.com"><span class="icon"><i class="fa fa-wordpress" aria-hidden="true"></i></span></a></div>
-			</div>
-	</section>
-</footer>
+    <section class="section section-header bg-gray">
+      <section class="grid-header container grid-960 text-center">
+        <p>maxezaki.com</p>
+      </section>
+    </section>
+    <div class="col-5 col-xs-11 wrapper">
+      <div class="panel">
+        <div class="panel-header text-center">
+          <figure class="avatar avatar-lg"><img src="images/icon_max00.jpg"></figure>
+          <div class="panel-title mt-10">Max_Ezaki</div>
+        </div>
+        <div class="panel-nav">
+          <ul class="tab tab-block">
+          <li class="tab-item active">
+            <a href="#">Profile</a>
+          </li>
+        </ul>
+        </div>
+        <div class="panel-body">
+          <p>Hi, this is Max Ezaki. Feel free to contact me :)<br>
+            <a href="https://www.pakutaso.com/Max_Ezaki.html">#stockphotomodel</a> <a>#webengineer</a> <a>#mc</a> <a href="https://www.pakutaso.com/search.html?offset=0&limit=30&search=%E3%83%89%E3%82%A4%E3%83%84%E4%BA%BA%E3%83%8F%E3%83%BC%E3%83%95">#現役ドイツ人ハーフ</a> <a href="https://www.pakutaso.com/">#ぱくたそ</a> <a href="#">#japnese</a> <a href="#">#english</a> <a href="#">#german</a>
+          </p>
+          <div class="text-center">
+          <button class="btn btn-primary" @click="openLink('https://form.jotform.me/Max_Ezaki/contact')"><i class="fa fa-envelope"></i> Contact</button>
+          </div>
+        </div>
+        <div class="panel-footer">
+        </div>
+      </div>
+    </div>
+    <footer class="bg-gray footer">
+      <section class="grid-footer container grid-960">
+          <div class="columns">
+            <div class="column text-center"><a href="https://www.facebook.com/rhythmonchi/"><span class="icon"><i class="fa fa-facebook" aria-hidden="true"></i></span></a></div>
+            <div class="column text-center"><a href="https://twitter.com/Max_Ezaki"><span class="icon"><i class="fa fa-twitter" aria-hidden="true"></i></span></a></div>
+            <div class="column text-center"><a href="http://maxezaki.photo"><span class="icon"><i class="fa fa-tumblr" aria-hidden="true"></i></span></a></div>
+            <div class="column text-center"><a href="https:/rhythm-onchi.com"><span class="icon"><i class="fa fa-wordpress" aria-hidden="true"></i></span></a></div>
+          </div>
+      </section>
+    </footer>
   </div>
 </template>
 
@@ -72,32 +50,13 @@
     data() {
       return {
 
-      panelists: ['Evan You', 'Chris Fritz'],
-      defaultPanelist: 'Evan You',
-
-        formContact: {
-            name: '',
-            email: '',
-            desc: ''
-        },
-
-        ruleValidate: {
-            name: [
-                { required: true, message: 'The name cannot be empty', trigger: 'blur' }
-            ],
-            email: [
-                { required: true, message: 'Mailbox cannot be empty', trigger: 'blur' },
-                { type: 'email', message: 'Incorrect email format', trigger: 'blur' }
-            ],
-            desc: [
-                { required: true, message: 'Please enter a personal introduction', trigger: 'blur' },
-                { type: 'string', min: 20, message: 'Introduce no less than 20 words', trigger: 'blur' }
-            ]
-        }
-
+      }
+    },
+    methods: {
+      openLink(url){
+        window.open = url;
       }
     }
-  }
 
 </script>
 
