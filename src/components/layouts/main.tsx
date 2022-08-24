08136c36ff674783caae258ab1a658bd54585f51
MaxEzaki <max.perc@gmail.com>
2022-08-24 18:18:51 +0900

    feat: フッターが最下部に行くように修正

--- src/components/layouts/main.tsx
+++ src/components/layouts/main.tsx
@@ -1,13 +1,21 @@
 import Header from '../organisms/Header';
 import Footer from '../organisms/Footer';
-
+import { css } from '@linaria/core';
+const wrapper = css`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  overflow: hidden;
+`;
 const Main = ({ children, router }) => {
   return (
-    <>
+    <div className={wrapper}>
       <Header />
       <main>{children}</main>
       <Footer />
-    </>
+    </div>
   );
 };
 
