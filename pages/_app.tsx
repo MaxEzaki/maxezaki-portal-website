758359962f00ce22f8b88ed377d40f03f46fa094
MaxEzaki <max.perc@gmail.com>
2022-08-26 16:36:27 +0900

    feat: 404追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,12 +14,13 @@ function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <>
       <Head>
+        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@800&family=Noto+Sans+JP:wght@400;700&display=swap"
           rel="stylesheet"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Max_Ezaki</title>
+        <title>Max_Ezaki - Official Website -</title>
       </Head>
       <Layout router={router}>
         <Component {...pageProps} key={router.route} />
