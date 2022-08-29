af1230383db73a1f8239b3be754050a622c5f6bd
MaxEzaki <max.perc@gmail.com>
2022-08-29 23:00:25 +0900

    feat: ogp設定

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,6 @@ import Layout from '../src/components/layouts/main';
 import '../styles/reset.css';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import css from 'linaria';
-
-// export const global = css`
-//   :global() {
-//   }
-// `;
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   return (
@@ -21,6 +15,22 @@ function MyApp({ Component, pageProps, router }: AppProps) {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Max_Ezaki - Official Website -</title>
+        <meta
+          name="description"
+          content="Max_Ezaki のオフィシャルウェブサイト"
+        />
+        <meta property="og:url" content="https://maxezaki.com" />
+        <meta
+          property="og:site_name"
+          content="Max_Ezaki - Official Website -"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Max_Ezaki - Official Website -" />
+        <meta
+          property="og:description"
+          content="Max_Ezaki のオフィシャルウェブサイト"
+        />
+        <meta property="og:image" content="/ogp.jpeg" />
       </Head>
       <Layout router={router}>
         <Component {...pageProps} key={router.route} />
