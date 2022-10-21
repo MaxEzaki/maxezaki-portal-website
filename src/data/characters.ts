6a05daaf35037d66da5bfebe50785527589b4e8b
MaxEzaki <max.perc@gmail.com>
2022-10-21 17:56:53 +0900

    feat: レンダリング方式変更とmcの写真追加
    ハッシュタグクリック時にページ遷移をやめた

---
+++ src/data/characters.ts
@@ -0,0 +1,69 @@
+export const characters = [
+  {
+    name: 'Max_Ezaki',
+    image: {
+      src: '/max_index.jpeg',
+      alt: 'Max_Ezaki Index',
+    },
+    description: `現役ドイツ人ハーフのマックスさんです。\n好きな食べ物はビールです。`,
+    type: 'index',
+  },
+  {
+    name: 'Max_Ezaki',
+    image: {
+      src: '/max_stockphoto.jpeg',
+      alt: 'Max_Ezaki StockPhoto Model',
+    },
+    description: `<a href="https://www.pakutaso.com/Max_Ezaki.html" target="_blank">ぱくたそ</a>という日本一のフリー素材サイトでフリー素材モデルやってます。使ってくれたら喜びます。`,
+    type: 'stockphoto',
+  },
+  {
+    name: 'Max_Ezaki',
+    image: {
+      src: '/max_mc.jpeg',
+      alt: 'Max_Ezaki MC',
+    },
+    description: `イベント司会、結婚式司会（日英含む）などたまにやってます。台本は必須アイテム。あとビール。`,
+    type: 'mc',
+  },
+  {
+    name: 'Max_Ezaki',
+    image: {
+      src: '/max_engineer.jpeg',
+      alt: 'Max_Ezaki Engineer',
+    },
+    description: `一応ITエンジニアやってます。PHP, Vue, Typescript あたりでしょうか。React はお触り中です。`,
+    type: 'engineer',
+  },
+  {
+    name: 'Max_Ezaki',
+    image: {
+      src: '/max_teacher.jpeg',
+      alt: 'Max_Ezaki Teacher',
+    },
+    description: `中学生メインで数学と理科を教えてました（英語ではない）。集団、個別どちらも対応可能。最後はイギリスで教えてました。`,
+    type: 'teacher',
+  },
+  {
+    name: 'Max_Ezaki',
+    image: {
+      src: '/max_percussionist.jpeg',
+      alt: 'Max_Ezaki Percussionist',
+    },
+    description: `<a href="https://bommer.me" target="_blank">BOMmER</a>というバンドで太鼓叩いてます。荷物量エグいんでライブのときはタクシー絶対。`,
+    type: 'percussionist',
+  },
+  {
+    name: 'Max_Ezaki',
+    image: {
+      src: '/max_actor.jpeg',
+      alt: 'Max_Ezaki Actor',
+    },
+    description: `動画もいけます。下記よりご視聴ください。
+    - <a href="https://vimeo.com/466401103" target="_blank">トレーラームービー</a>
+    - <a href="https://vimeo.com/466400923" target="_blank">動画広告</a>
+    - <a href="https://vimeo.com/466400672" target="_blank">【ドクターシーラボ公式】商品紹介</a>
+    <a href="https://soundcloud.com/max-ezaki/sets/voice-sample-from-trailer-movie-remind?si=9a77e471d4bc4c77ae56485e12b9a471&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" target="_blank">ボイスサンプル</a>もあります`,
+    type: 'actor',
+  },
+];
---
+++ src/data/hashtags.ts
@@ -0,0 +1,38 @@
+export const hashtags = [
+  {
+    id: 1,
+    name: 'フリー素材',
+    link: '/stockphoto',
+    value: 'stockphoto',
+  },
+  {
+    id: 2,
+    name: '司会',
+    link: '/mc',
+    value: 'mc',
+  },
+  {
+    id: 3,
+    name: 'ITエンジニア',
+    link: '/engineer',
+    value: 'engineer',
+  },
+  {
+    id: 4,
+    name: '塾講師',
+    link: '/teacher',
+    value: 'teacher',
+  },
+  {
+    id: 5,
+    name: 'パーカショニスト',
+    link: '/percussionist',
+    value: 'percussionist',
+  },
+  {
+    id: 6,
+    name: '演者',
+    link: '/actor',
+    value: 'actor',
+  },
+];
