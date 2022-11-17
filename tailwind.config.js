19ea6295c6742dca7139d9a801385271ece5689b
MaxEzaki <max.perc@gmail.com>
2022-11-17 23:46:12 +0900

    feat: set tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
+  mode: "jit",
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      blue: "#2584c8",
+    },
+    fontFamily: {
+      sans: ["Noto Sans JP"],
+    },
     extend: {},
   },
   plugins: [],
