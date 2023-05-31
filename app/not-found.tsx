394ac629cbdcbc49020e45fcf30a93816f26be13
MaxEzaki <max.perc@gmail.com>
2023-05-31 12:36:37 +0900

    feat: got to top 追加

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,14 @@
 import styles from "./page.module.css";
 import Card from "./_components/card";
+import Link from "next/link";
 
 const NotFound = () => {
   return (
     <div className={styles.wrapper}>
       {/* <Card type="404" /> */}
-      404 not found page
+      <p>404 not found page </p>
+      <br />
+      <Link href={"/"}>go to top</Link>
     </div>
   );
 };
