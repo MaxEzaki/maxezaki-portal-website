cc547d7a2b2b72436e8404bc8391ffec123d23e3
MaxEzaki <max.perc@gmail.com>
2023-05-31 12:33:26 +0900

    feat: 動作確認

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,8 @@ import Card from "./_components/card";
 const NotFound = () => {
   return (
     <div className={styles.wrapper}>
-      <Card type="404" />
+      {/* <Card type="404" /> */}
+      404 not found page
     </div>
   );
 };
