f40b9299eafe9b3219c9aa91f0d887fa645d36b8
MaxEzaki <max.perc@gmail.com>
2023-06-03 00:35:12 +0900

    feat: スマホ版スタイルの調整

--- app/_data/movies.ts
+++ app/_data/movies.ts
@@ -2,16 +2,16 @@ export const movies = [
   {
     id: 1,
     embed: `<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/466401103?h=43249f5978" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
-    <p><a href="https://vimeo.com/466401103">Re-MIND Official Trailer</a> from <a href="https://vimeo.com/maxezaki">Max Ezaki</a> on <a href="https://vimeo.com">Vimeo</a>.</p>`,
+    <p><a href="https://vimeo.com/466401103">Re-MIND Official Trailer</a></p>`,
   },
   {
     id: 2,
     embed: `<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/466400923?h=1f73c774ef" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
-    <p><a href="https://vimeo.com/466400923">フリーランスが自分らしい働き方を実現できる『PROsheet』</a> from <a href="https://vimeo.com/maxezaki">Max Ezaki</a> on <a href="https://vimeo.com">Vimeo</a>.</p>`,
+    <p><a href="https://vimeo.com/466400923">フリーランスが自分らしい働き方を実現できる『PROsheet』</a></p>`,
   },
   {
     id: 3,
     embed: `<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/466400672?h=505a12da3a" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
-    <p><a href="https://vimeo.com/466400672">【ドクターシーラボ公式】今こそ美白に無料でチャレンジ！美白トライアルキット0円でもらえる！</a> from <a href="https://vimeo.com/maxezaki">Max Ezaki</a> on <a href="https://vimeo.com">Vimeo</a>.</p>`,
+    <p><a href="https://vimeo.com/466400672">【ドクターシーラボ公式】今こそ美白に無料でチャレンジ！美白トライアルキット0円でもらえる！</a></p>`,
   },
 ];
