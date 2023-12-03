936574b32f192c505ecebf0f8e32af2e67666b00
MaxEzaki <max.perc@gmail.com>
2023-12-03 22:56:45 +0900

    githubのリンクを追加した

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,10 +1,16 @@
 import Link from "next/link";
 import Image from "next/image";
 import styles from "./header.module.css";
+import { FaGithub } from "react-icons/fa";
 
 const navLinks = [
-  { href: "/interview", label: "Interview" },
-  { href: "/movie", label: "Movie" },
+  { href: "/interview", label: "Interview", icon: null },
+  { href: "/movie", label: "Movie", icon: null },
+  {
+    href: "https://github.com/MaxEzaki/maxezaki-portal-website",
+    label: "Source",
+    icon: <FaGithub size={"1em"} title={"github"} className="mr-1" />,
+  },
 ];
 
 const Header = () => {
@@ -20,7 +26,13 @@ const Header = () => {
           />
         </Link>
         {navLinks.map((link) => (
-          <Link key={link.href} href={link.href}>
+          <Link
+            key={link.href}
+            href={link.href}
+            className="inline-flex items-center justify-center hover:underline"
+            target={link.icon ? "_blank" : undefined}
+          >
+            {link.icon}
             {link.label}
           </Link>
         ))}
