ff253de9911557dbc4f199849bb008fc0bafd8b4
MaxEzaki <max.perc@gmail.com>
2025-02-18 11:10:40 +0900

    feat: インタビュー記事１つ削除

--- app/_data/interviews.ts
+++ app/_data/interviews.ts
@@ -17,18 +17,12 @@ export const interviews: Interview[] = [
   },
   {
     id: 3,
-    title: "辞めたいと思ったら辞めればいいフリー素材モデルの人生観を聞いてみた",
-    link: "https://www.taishokudaikou.com/reboot/archive/423/",
-    image: "/2019_01_28_ezaki_01-1024x768.jpg",
-  },
-  {
-    id: 4,
     title: "フリー素材のモデルってどんな仕事 現役ドイツ人ハーフに聞いてきた",
     link: "https://sidelines.jp/post-3114",
     image: "/top_MAX-thumb-815x400-4956-1.jpg",
   },
   {
-    id: 5,
+    id: 4,
     title: "極貧ドイツ留学生がマルチエンジニアに今日とは違う明日の生き方",
     link: "https://sidelines.jp/post-3117",
     image: "/max160227590I9A1334_TP_V-1024x682.jpg",
